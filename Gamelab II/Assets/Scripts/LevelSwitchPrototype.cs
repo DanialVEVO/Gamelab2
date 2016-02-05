c24012f22c483a518a893b307cfe28d18f2e8bdd
svenevertse <svenevertse@gmail.com>
2016-02-05 11:02:32 +0100

    Update Level Switch

---
+++ Gamelab II/Assets/Scripts/LevelSwitchPrototype.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitchPrototype : MonoBehaviour {
+
+	public GameObject [] levels;
+
+	public Transform levelSpawn;
+	public Transform playerSpawn;
+
+	public int currentKamer;
+	public int kamerCheck;
+	public int maxCount1, maxCount2, maxCount3;
+	public int minCount1, minCount2, minCount3;
+
+	void Start () {
+		Instantiate(levels[0], levelSpawn.position, Quaternion.identity);
+	}
+	
+	void Update () {
+	
+	}
+
+	public void SwitchLevel (int switcher) {
+		switch (switcher){
+			case 1 : 
+				currentKamer = Random.Range(minCount1, maxCount1);
+				SpawnLevel();
+			break;
+
+			case 2 : 
+				currentKamer = Random.Range(minCount2, maxCount2);
+				SpawnLevel();
+			break;
+
+			case 3 : 
+				currentKamer = Random.Range(minCount3, maxCount3);
+				SpawnLevel();
+			break;
+		}
+	}
+
+	void SpawnLevel (){
+		Instantiate(levels[currentKamer], levelSpawn.position, Quaternion.identity);
+		print(currentKamer);
+		Destroy(GameObject.FindWithTag("CurrentLevel"));
+		transform.position = playerSpawn.transform.position;
+	}
+
+	public void OnCollisionEnter (Collision col){
+		if(col.transform.name == "NextLevelKamer1"){
+			kamerCheck = 1;
+			SwitchLevel(kamerCheck);
+		}
+
+		if(col.transform.name == "NextLevelKamer2"){
+			kamerCheck = 2;
+			SwitchLevel(kamerCheck);
+		}
+
+		if(col.transform.name == "NextLevelKamer3"){
+			kamerCheck = 3;
+			SwitchLevel(kamerCheck);
+		}
+	}
+}
