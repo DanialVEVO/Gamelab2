49c9381e661b4f631ba092f3c536b74667a84625
svenevertse <svenevertse@gmail.com>
2016-02-23 12:49:14 +0100

    Teleporter Prototype

---
+++ Gamelab II Prototype/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public Vector3 teleportPos;
+	public float minPosX, maxPosX, minPosZ, maxPosZ, maxYPos;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter (Collider trigger){
+		if(trigger.transform.tag == "Player"){
+			teleportPos.x = Random.Range (minPosX, maxPosX);
+			teleportPos.y = maxYPos;
+			teleportPos.z = Random.Range (minPosZ, maxPosZ);
+			trigger.transform.position = teleportPos;
+		}
+	}
+}
