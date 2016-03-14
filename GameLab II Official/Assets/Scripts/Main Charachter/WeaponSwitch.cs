a6b5f136ecc7c13c6265f52dd6004544a9017fb8
svenevertse <svenevertse@gmail.com>
2016-03-14 11:30:16 +0100

    WeaponSwitch Almost done

---
+++ GameLab II Official/Assets/Scripts/Main Charachter/WeaponSwitch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSwitch : MonoBehaviour {
+
+	public int curWeapon;
+
+	public GameObject [] weapons;
+
+
+	void Start () {
+	
+		weapons[0].SetActive(true);
+
+	}
+	
+	void Update () {
+		
+		WeaponSwitcher ();
+
+	}
+
+	public void WeaponSwitcher () {
+
+		if(Input.GetButtonDown("SwitchWeapon")){
+			curWeapon ++;
+			for(int i = 0; i < weapons.Length; i ++){
+				if(curWeapon == i){
+					weapons[i].SetActive(true);
+					print(i);
+				}
+				else{
+					weapons[i].SetActive(false);
+				}
+			}
+			if(curWeapon == weapons.Length){
+				curWeapon = 0;
+			}
+		}
+	}
+}
