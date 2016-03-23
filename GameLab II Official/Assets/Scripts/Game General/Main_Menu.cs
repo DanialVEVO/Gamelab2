59a6e5cef972a012684dc7c9b7d9fe2d11a22362
svenevertse <svenevertse@gmail.com>
2016-03-23 16:23:53 +0100

    Main Menu Start

---
+++ GameLab II Official/Assets/Scripts/Game General/Main_Menu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_Menu : MonoBehaviour {
+
+	public GameObject mainMenuButtons;
+	public GameObject optionsMenuButtons;
+	public GameObject creditsMenu;
+
+	void Start () {
+
+		mainMenuButtons.SetActive(true);
+		optionsMenuButtons.SetActive(false);
+		creditsMenu.SetActive(false);
+
+		AssignButtons();
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void StartGame (){
+
+		Application.LoadLevel(1);
+
+	}
+
+	public void OpenOptions (){
+
+		mainMenuButtons.SetActive(false);
+		optionsMenuButtons.SetActive(true);
+
+	}
+
+	public void ReturnToMainMenu (){
+
+		mainMenuButtons.SetActive(true);
+		optionsMenuButtons.SetActive(false);
+		creditsMenu.SetActive(false);
+
+	}
+
+	public void OpenCredits (){
+
+		creditsMenu.SetActive(true);
+		mainMenuButtons.SetActive(false);
+
+	}
+
+	public void QuitToDestop (){
+
+		Application.Quit();
+		print("QuitToDestop");
+	}
+
+	public void AssignButtons (){
+
+		mainMenuButtons = GameObject.Find("MainMenuButtons");
+		optionsMenuButtons = GameObject.Find("OptionMenuButtons");
+		creditsMenu = GameObject.Find("Credits Menu");
+
+
+	}
+}
