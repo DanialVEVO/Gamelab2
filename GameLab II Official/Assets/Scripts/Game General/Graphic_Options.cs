0059c899cc84874e18d923a49602ca1e1239361d
svenevertse <svenevertse@gmail.com>
2016-04-01 08:51:42 +0200

    Start Graphic Settings

--- GameLab II Official/Assets/Scripts/Game General/Graphic_Options.cs
+++ GameLab II Official/Assets/Scripts/Game General/Graphic_Options.cs
@@ -3,16 +3,37 @@ using System.Collections;
 
 public class Graphic_Options : MonoBehaviour {
 
-	public string [] qualityNames = QualitySettings.names;
-
+	public int qualityLevel;
 
 	void Start () {
 	
+	qualityLevel = QualitySettings.GetQualityLevel();	
 
 	}
 	
 	void Update () {
 	
+	}
+
+	public void QualityUp (){
+
+		string[] names = QualitySettings.names;
+		GUILayout.BeginVertical();
+		int i = 0;
+		while (i < names.Length){
+			if(GUILayout.Button(names[1])){
+				QualitySettings.SetQualityLevel(i, true);
+			}
+			i ++;
+		}
+		GUILayout.EndVertical();
+	}
+
+	public void QualityDown (){
+
+		qualityLevel -= 1;
+		print(qualityLevel);
+
 	}
 
 }
