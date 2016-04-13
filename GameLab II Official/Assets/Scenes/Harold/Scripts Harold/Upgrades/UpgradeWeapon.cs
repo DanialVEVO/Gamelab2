1bd39a908df7b9df44a7fe376b5a7373f79686a7
HaroldGaming <harold-de-jong@hotmail.com>
2016-04-13 13:56:17 +0200

    Scripts Update
    Scripts aangemaakt

---
+++ GameLab II Official/Assets/Scenes/Harold/Scripts Harold/Upgrades/UpgradeWeapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeWeapon : MonoBehaviour {
+
+	public enum UpGrade{
+		Upgrade0,
+		Upgrade1,
+		Upgrade2,
+		Upgrade3,
+		Upgrade4,
+		Upgrade5
+	}
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	public void Upgrade(){
+		
+	}
+}
