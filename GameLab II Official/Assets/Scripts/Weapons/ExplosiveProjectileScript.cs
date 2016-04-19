b23d482c438ee289a2b0fb7fd0d9e334b090e8d7
Danial <danial.azimi.a@gmail.com>
2016-04-19 14:38:03 +0200

    Grenade launcher script

--- GameLab II Official/Assets/Scripts/Weapons/ExplosiveProjectileScript.cs
+++ GameLab II Official/Assets/Scripts/Weapons/ExplosiveProjectileScript.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/* [Code]
+ * Explosives Class
+ * Scripted by Danial
+ */
+
+using UnityEngine;
 using System.Collections;
 
 public class ExplosiveProjectileScript : MonoBehaviour {
@@ -9,24 +14,31 @@ public class ExplosiveProjectileScript : MonoBehaviour {
 						}
 	public	Explosives	myExplosiveType;
 
-	public	float		fuseTimer;
+	public	float		fuseTimer = 3f;
+	public	float		power = 50;
+	public	float		radius = 25;
+	public	float		playerDamage = 20f;
+
+	public	int			enemyDamage = 10;
+
+	public	GameObject	particle;
 
 	public	RaycastHit	hit;
 
 
 	// Use this for initialization
-	void Start () {
+	void Start(){
 
-		IdentifyMe();
+		
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update(){
+		IdentifyMe();		
 	}
 
-	void IdentifyMe () {
+	public void IdentifyMe(){
 		switch(myExplosiveType){
 
 			case Explosives.Grenade :
@@ -42,11 +54,59 @@ public class ExplosiveProjectileScript : MonoBehaviour {
 
 	}
 
-	void Grenade () {
-		print("myExplosiveType");
+	public void Grenade(){
+		//print("myExplosiveType");
+		fuseTimer -= Time.deltaTime;
+		if(fuseTimer <= 0){
+			GrenadeExplode();
+		}
 	}
 
-	void Rocket (){
-		print("myExplosiveType");
+	public void Rocket(){
+		//print("myExplosiveType");
 	}
+
+	public void GrenadeExplode(){
+		Explode();
+		// Destroy(gameObject);
+	}
+
+	public void RocketExplode(){
+
+	}
+
+	public void Explode(){
+		Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach(Collider hit in colliders){
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+			if(rb != null){
+				rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+			} else {
+				continue;
+			}
+			if(rb.transform.tag == "Enemy" || rb.transform.tag == "FlyingEnemy"){
+				GiveEnemyDamage(enemyDamage, rb);
+			}
+
+			if(rb.transform.tag == "Player"){
+				GivePlayerDamage(playerDamage, rb);
+			}
+		}
+		DestroyMe();
+	}
+
+	public void DestroyMe(){
+		Destroy(gameObject);
+	}
+
+	public void GiveEnemyDamage(int damage, Rigidbody rb){
+		rb.transform.GetComponent<EnemyBaseClass>().Health(damage);
+	}
+
+	public void GivePlayerDamage(float damage, Rigidbody rb){
+		rb.transform.GetComponent<Health_TakeDamage_HitLocation>().HealthCalculator(damage);
+	}
+
+
 }
---
+++ GameLab II Official/Assets/Scripts/World Building/ParticleDestroyScript.cs
@@ -0,0 +1,17 @@
+﻿/* [no code]
+ * Particle Destroyer Script
+ * Scripted by Danial
+ */
+using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroyScript : MonoBehaviour {
+	
+	void Start () {
+		ParticleDestroy();
+	}
+	
+	public void ParticleDestroy () {
+		Destroy(gameObject, GetComponent<ParticleSystem>().duration);
+	}
+}
