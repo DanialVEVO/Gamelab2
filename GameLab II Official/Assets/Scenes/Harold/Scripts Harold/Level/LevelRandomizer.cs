f8e448261e02a5d1e1d5ac1859857656cf552fd5
Sven <svenevertse@gmail.com>
2016-04-19 15:45:38 +0100

    LEVEL IMPLEMENTED

--- GameLab II Official/Assets/Scenes/Harold/Scripts Harold/Level/LevelRandomizer.cs
+++ GameLab II Official/Assets/Scenes/Harold/Scripts Harold/Level/LevelRandomizer.cs
@@ -18,35 +18,35 @@ public class LevelRandomizer : LevelSpawn {
 		switch (levelSpawner.levelCounter){
 		case 0:
 			print("Picked");
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, levelListEasy.Count);
 			levelSpawner.levelPicks[0] = levelListEasy[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, levelListMedium.Count);
 			levelSpawner.levelPicks[1] = levelListMedium[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, levelListHard.Count);
 			levelSpawner.levelPicks[2] = levelListHard[randomNum];
 			break;
 
 		case 1:
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level2ListEasy.Count);
 			levelSpawner.levelPicks[0] = level2ListEasy[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level2ListMedium.Count);
 			levelSpawner.levelPicks[1] = level2ListMedium[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level2ListHard.Count);
 			levelSpawner.levelPicks[2] = level2ListHard[randomNum];
 			break;
 
 		case 2:
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level3ListEasy.Count);
 			levelSpawner.levelPicks[0] = level3ListEasy[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level3ListMedium.Count);
 			levelSpawner.levelPicks[1] = level3ListMedium[randomNum];
 
-			randomNum = Random.Range(0, 2);
+			randomNum = Random.Range(0, level3ListHard.Count);
 			levelSpawner.levelPicks[2] = level3ListHard[randomNum];
 			break;
 		}
