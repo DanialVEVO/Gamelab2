ab36d10c4ddb7eb935e93c75fdfb26fd7c49c22a
Sven <svenevertse@gmail.com>
2016-04-22 08:55:04 +0100

    Weapon Script Implemented

--- GameLab II Official/Assets/Scripts/Level USP/Teleporter.cs
+++ GameLab II Official/Assets/Scripts/Level USP/Teleporter.cs
@@ -17,12 +17,8 @@ public class Teleporter : MonoBehaviour {
 
 	public void OnTriggerEnter (Collider trigger){
 		if(trigger.transform.tag == "Player"){
-			for(int i = 0; i < teleporterPositions.Length; i ++){
 				randomChecker = Random.Range(0, teleporterPositions.Length);
-				if(i == randomChecker){
-					trigger.transform.position = teleporterPositions[i].position;
-				}
-			}
+				trigger.transform.position = teleporterPositions[randomChecker].position;
 		}
 	}
 }
