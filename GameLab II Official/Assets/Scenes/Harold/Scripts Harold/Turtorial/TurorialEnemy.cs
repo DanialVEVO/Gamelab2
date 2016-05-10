81105405dd4d15e29cd70510d7d3fb823a7ab6ef
HaroldGaming <harold-de-jong@hotmail.com>
2016-05-10 16:38:43 +0200

    Turtorial Progress

---
+++ GameLab II Official/Assets/Scenes/Harold/Scripts Harold/Turtorial/TurorialEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurorialEnemy : MonoBehaviour {
+
+	public GameObject turtorialEnemy;
+	public Vector3 spawnPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Respawner(){
+		Instantiate(turtorialEnemy, spawnPosition, transform.rotation);
+	}
+}
