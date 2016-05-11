48000e25d3e384e9385faa43138dd57f97cd1797
Danial <danial.azimi.a@gmail.com>
2016-05-11 13:17:48 +0200

    Spawner

--- GameLab II Official/Assets/Scripts/World Building/Balancer.cs
+++ GameLab II Official/Assets/Scripts/World Building/Balancer.cs
@@ -4,18 +4,29 @@
  */
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Balancer : MonoBehaviour {
 
-	public int	weightLimit;
+	public	int	weightLimit;
+			int currentWeight;
+	public	int	chosenEnemy;
+	public List<Transform> enemyList = new List<Transform>();
 
 	// Use this for initialization
 	void Start () {
-	
+	 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(currentWeight <= currentWeight){
+			Debug.Log(ChooseEnemy());
+		}
+	}
+
+	public int ChooseEnemy () {
+		chosenEnemy = Random.Range(0, enemyList.Count);
+		return chosenEnemy;
 	}
 }
--- GameLab II Official/Assets/Scripts/World Building/MakeEnemy.cs
+++ GameLab II Official/Assets/Scripts/World Building/MakeEnemy.cs
@@ -1,43 +1,45 @@
-﻿/* [Code]
- * Enemy Creation Class
- * Scripted by Danial
- */
-using UnityEngine;
-using System.Collections;
+﻿// /* [Code]
+//  * Enemy Creation Class
+//  * Scripted by Danial
+//  */
+// using UnityEngine;
+// using System.Collections;
 
-public class MakeEnemy : MonoBehaviour {
+// public class MakeEnemy : MonoBehaviour {
 
-	public	bool	flying;
-	public	bool	walking;
-	public	bool	melee;
-	public	bool	shooting;
+// 	public	bool	flying;
+// 	public	bool	walking;
+// 	public	bool	melee;
+// 	public	bool	shooting;
 
-	public	int		minChance = 0;
-	public	int		maxChance = 100;
-	public	int		rolledChance;
+// 	public	int		minChance = 0;
+// 	public	int		maxChance = 100;
+// 	public	int		rolledChance;
 
-	public	int		chanceMelee;
-	public	int		chanceShooting;
+// 	public	int		chanceMelee;
+// 	public	int		chanceShooting;
 
-	public	int		chanceChampion;
-	public	int		chanceNormal;
-	public	int		chanceMidget;
+// 	public	int		chanceChampion;
+// 	public	int		chanceNormal;
+// 	public	int		chanceMidget;
 
-	public	int		weight;
+// 	public	int		weight;
+// 	public	GameObject	levelManager;
 
-	// Use this for initialization
-	void Start () {
-
-	}
+// 	// Use this for initialization
+// 	void Start () {
+		
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
+// 	// Update is called once per frame
+// 	void Update () {
+// 		balancer.weightLimit += 1;
+// 	}
 
-	}
+// 	public void RollEnemey (){
+// 		Component balancer = levelManager.GetComponent<Balancer>();
+// 		if(walking == true){
 
-	public void RollEnemey (){
-		if(walking == true){
-
-		}
-	}
-}
+// 		}
+// 	}
+// }
