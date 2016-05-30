1e7c4d1c02d98105b2c942c02f8e177bf46228e5
HaroldGaming <harold-de-jong@hotmail.com>
2016-05-30 11:35:49 +0200

    Hud weapon select script gemaakt

---
+++ GameLab II Official/Assets/Scenes/Harold/Scripts Harold/Hud/WeaponSelect.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSelect : MonoBehaviour {
+
+	public GameObject[] weaponBarsObject;
+	public string[] weaponNameList;
+	public string weaponCounterString;
+	public int listCounter, weaponCounter;
+	public GameObject currentWeapon;
+
+
+	void Start () {
+		listCounter = 1;
+	}
+
+	void Update () {
+	
+	}
+
+	public void WeaponLister(){
+		weaponCounterString = string.Format("{0}", listCounter);
+		if(currentWeapon.tag == weaponCounterString){
+			weaponBarsObject[listCounter].transform.FindChild(weaponNameList[listCounter]).gameObject.SetActive(true);
+			listCounter++;
+		}
+	}
+
+	public void SelectedWeapon(){
+		SetHighlightOff();
+		weaponCounterString = string.Format("{0}", weaponCounter);
+		if(currentWeapon.tag == weaponCounterString){
+			weaponBarsObject[weaponCounter].transform.FindChild("").gameObject.SetActive(true);//HighLighter name
+		}	
+	}
+
+	void SetHighlightOff(){
+		for(int i = 0; i < weaponBarsObject.Length; i++){
+			weaponBarsObject[i].transform.FindChild("").gameObject.SetActive(false);//HighLighter name
+		}
+	}
+}
