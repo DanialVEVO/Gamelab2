4bb4cbe532211b77f66853341f174c039d510b8f
Danial <danial.azimi.a@gmail.com>
2016-06-14 15:12:40 +0200

    Teleporter

--- GameLab II Official/Assets/Scripts/Level USP/Teleporter.cs
+++ GameLab II Official/Assets/Scripts/Level USP/Teleporter.cs
@@ -16,6 +16,7 @@ public class Teleporter : MonoBehaviour {
 	public void OnTriggerEnter (Collider trigger){
 		if(trigger.transform.tag == "Player"){
 			trigger.transform.position = targetPoint.position;
+			trigger.transform.rotation = targetPoint.rotation;
 		}
 	}
 }
