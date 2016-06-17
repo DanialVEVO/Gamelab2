48cbe4fb8c171e195aab58a5913b7ec6af0ad1b5
Sven <svenevertse@gmail.com>
2016-06-17 10:31:46 +0100

    ShopKeeper Animations Scripted

---
+++ GameLab II Official/Assets/Scripts/Animations/NPCAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCAnimation : MonoBehaviour {
+
+	public Animator anim;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider trigger) {
+
+		if(trigger.transform.name == "PlayerTest"){
+			anim.SetBool("IfDepspawn", false);
+			anim.SetBool("IfSpawn", true);
+			anim.SetBool("Idle", true);
+		}
+	}
+
+	void OnTriggerExit (Collider trigger) {
+		if(trigger.transform.name == "PlayerTest"){
+			anim.SetBool("Idle", false);
+			anim.SetBool("IfSpawn", false);
+			anim.SetBool("IfDepspawn", true);
+		}
+	}
+}
