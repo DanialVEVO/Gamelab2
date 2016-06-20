392acc637bb3c71399ad7f3ee4b549c9f59b8eb9
Danial <danial.azimi.a@gmail.com>
2016-06-20 09:15:06 +0200

    Pickup asset imports

---
+++ GameLab II Official/Assets/Scripts/Pickups/RotateAndFloat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAndFloat : MonoBehaviour {
+
+	public float speed = 400f;
+	public float openneer = 0f;
+
+	void Update (){
+		SineBounce();
+	}
+
+	public void SineBounce () {
+		Vector3 nextRotate = Vector3.zero;
+
+		nextRotate.y = speed * Time.deltaTime;
+		nextRotate.z = speed * Time.deltaTime;   
+
+		transform.Rotate(nextRotate);
+
+		openneer+=Time.deltaTime;
+		gameObject.transform.position=new Vector3(gameObject.transform.position.x, (Mathf.Sin(openneer)*0.1f)+0.85f, gameObject.transform.position.z);
+	}
+}
