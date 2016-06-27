f03ad1fe514833af3572c4444159cd5e5062ce0f
Sven <svenevertse@gmail.com>
2016-06-27 10:45:38 +0100

    Weapons Implemented

---
+++ GameLab II Official/Assets/Scripts/Animations/EnemieAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemieAnimation : MonoBehaviour {
+
+	public Animator enemyAnim;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void MummyShoot (){
+		
+	}
+}
