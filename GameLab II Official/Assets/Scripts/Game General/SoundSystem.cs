ee492f21066933a394cb295b30969bef41a51f30
Sven <svenevertse@gmail.com>
2016-06-30 17:17:15 +0100

    Ambient Sound System Done

--- GameLab II Official/Assets/Scripts/Game General/SoundSystem.cs
+++ GameLab II Official/Assets/Scripts/Game General/SoundSystem.cs
@@ -3,12 +3,31 @@ using System.Collections;
 
 public class SoundSystem : MonoBehaviour {
 
+	public float repeatRateAmbient, initialAmbientDelay;
+	public float volume;
+
+	public int randomAmbient;
+
+	public AudioClip [] ambientSounds;
+	public AudioSource audio;
+
 	void Start () {
-	
+
+		audio = GetComponent<AudioSource>();
+
+		InvokeRepeating("AmbientSound", initialAmbientDelay, repeatRateAmbient);
 	}
 	
 	void Update () {
 	
+	}
+
+	void AmbientSound () {
+
+		randomAmbient = Random.Range(0, ambientSounds.Length);
+		print(randomAmbient);
+		//audio.PlayOneShot(ambientSounds[randomAmbient], volume);
+
 	}
 
 	
